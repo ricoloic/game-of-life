a45cd1dbd546c8e3f8aa90f03a58522ccb9c556a
ricoloic <ricoloic4@gmail.com>
2021-05-27 23:33:46 -0400

    started grid array and cell object, and changing of cell state on mousePressed

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,70 @@
+function Cell(x, y, xSize, ySize, columnIndex, rowIndex, isAlive = false) {
+	this.x = x, this.y = y, this.xSize = xSize, this.ySize = ySize, this.columnIndex = columnIndex, this.rowIndex = rowIndex, this.isAlive = isAlive;
+}
+
+Cell.prototype.contains = function (x, y) {
+	if (x >= this.x && x <= this.x + this.xSize && y >= this.y && y <= this.y + this.ySize) return true;
+	return false;
+}
+
+Cell.prototype.show = function () {
+	if (this.isAlive) fill(0);
+	else fill(255);
+	rect(this.x, this.y, this.xSize, this.ySize);
+}
+
+Cell.prototype.changeState = function () {
+	this.isAlive = !this.isAlive;
+}
+
+function make2DArray(cols, rows) {
+	const arr = new Array(cols), xSize = width / cols, ySize = height / rows;
+	for (let i = 0; i < cols; i++) {
+		arr[i] = new Array(rows);
+		for (let j = 0; j < rows; j++) {
+			arr[i][j] = new Cell(i * xSize, j * ySize, xSize, ySize, i, j);
+		}
+	}
+	return arr;
+}
+
+const columns = 20, rows = 10, width = 1000, height = 500;
+let grid;
+
+function findCell(x, y) {
+	for (let i = 0; i < columns; i++) {
+		for (let j = 0; j < rows; j++) {
+			if (grid[i][j].contains(mouseX, mouseY)) return grid[i][j];
+		}
+	}
+	return null;
+}
+
+function updateGrid() {
+	for (let i = 0; i < columns; i++) {
+		for (let j = 0; j < rows; j++) {
+			grid[i][j].show();
+		}
+	}
+}
+
 function setup() {
-	createCanvas(600, 400);
-};
+	grid = make2DArray(columns, rows);
+	createCanvas(width, height);
+	strokeWeight(2);
+	for (let i = 0; i <= columns; i++) {
+		line(width / columns * i, 0, width / columns * i, height);
+		for (let j = 0; j <= rows; j++) {
+			line(0, height / rows * j, width, height / rows * j);
+		}
+	}
+}
+
+function mousePressed() {
+	const clickedCell = findCell(mouseX, mouseY);
+	if (clickedCell) clickedCell.changeState();
+}
 
 function draw() {
-	background(0);
-};
-
-console.log('hi from sketch.js')
+	updateGrid();
+}
