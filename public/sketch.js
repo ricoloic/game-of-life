8d1aaec1cf1e2ded0385c86f30a965a609a44f85
ricoloic <ricoloic4@gmail.com>
2021-05-28 02:00:33 -0400

    started working on the game itself

--- public/sketch.js
+++ public/sketch.js
@@ -18,14 +18,30 @@ Cell.prototype.contains = function (x, y) {
 	return false;
 }
 
+Cell.prototype.getColumnIndex = function () {
+	return this.columnIndex;
+}
+
+Cell.prototype.getRowIndex = function () {
+	return this.rowIndex;
+}
+
 Cell.prototype.show = function () {
 	if (this.isAlive) fill(0);
 	else fill(255);
 	rect(this.x, this.y, this.xSize, this.ySize);
 }
 
+Cell.prototype.update = function(nbrOfAliveNeighbourCells) {
+	if (nbrOfAliveNeighbourCells) {
+		if (this.isAlive && nbrOfAliveNeighbourCells < 2) this.changeState();
+		else if (this.isAlive && nbrOfAliveNeighbourCells > 3) this.changeState();
+		else if (!this.isAlive && nbrOfAliveNeighbourCells === 3) this.changeState();
+	}
+}
+
 Cell.prototype.changeState = function () {
-	this.isAlive = !this.isAlive;
+		this.isAlive = !this.isAlive;
 }
 
 function make2DArray(cols, rows) {
@@ -36,33 +52,79 @@ function make2DArray(cols, rows) {
 
 	for (let i = 0; i < cols; i++) {
 		arr[i] = new Array(rows);
-		for (let j = 0; j < rows; j++) {
+		for (let j = 0; j < rows; j++)
 			arr[i][j] = new Cell(i * xSize, j * ySize, xSize, ySize, i, j);
-		}
 	}
 
 	return arr;
 }
 
-const columns = 20, rows = 10, width = 1000, height = 500;
-let grid;
+const columns = 130, rows = 65, width = 1000, height = 500;
+let grid, pause = false, pauseButton;
 
 function findCell(x, y) {
 	for (let i = 0; i < columns; i++) {
-		for (let j = 0; j < rows; j++) {
+		for (let j = 0; j < rows; j++)
 			if (grid[i][j].contains(mouseX, mouseY)) return grid[i][j];
-		}
 	}
 	return null;
 }
 
-function getNeighbourCells() {
+function isValidNeighbourCell(columnIndex, rowIndex) {
+	if (grid[columnIndex] && grid[columnIndex][rowIndex])
+		return true;
+	return false;
+}
+
+function getNeighbourCells(cell) {
+	const
+		neighbourCells = [],
+		columnIndex = cell.getColumnIndex(),
+		rowIndex = cell.getRowIndex();
+	neighbourCells.push(
+		isValidNeighbourCell(columnIndex - 1, rowIndex - 1)
+			? grid[columnIndex - 1][rowIndex - 1] : null);
+	neighbourCells.push(
+		isValidNeighbourCell(columnIndex, rowIndex - 1)
+			? grid[columnIndex][rowIndex - 1] : null);
+	neighbourCells.push(
+		isValidNeighbourCell(columnIndex + 1, rowIndex - 1)
+			? grid[columnIndex + 1][rowIndex - 1] : null);
+	neighbourCells.push(
+		isValidNeighbourCell(columnIndex - 1, rowIndex)
+			? grid[columnIndex - 1][rowIndex] : null);
+	neighbourCells.push(
+		isValidNeighbourCell(columnIndex + 1, rowIndex)
+			? grid[columnIndex + 1][rowIndex] : null);
+	neighbourCells.push(
+		isValidNeighbourCell(columnIndex - 1, rowIndex + 1)
+			? grid[columnIndex - 1][rowIndex + 1] : null);
+	neighbourCells.push(
+		isValidNeighbourCell(columnIndex, rowIndex + 1)
+			? grid[columnIndex][rowIndex + 1] : null);
+	neighbourCells.push(
+		isValidNeighbourCell(columnIndex + 1, rowIndex + 1)
+			? grid[columnIndex + 1][rowIndex + 1] : null);
+	return neighbourCells;
+}
+
+function evaluateNeighbourCells(neighbourCells) {
+	let alive = 0;
+	for (let i = 0; i < neighbourCells.length; i++)
+		if (neighbourCells[i]?.isAlive) alive += 1;
+	return alive;
 }
 
 function updateGrid() {
 	for (let i = 0; i < columns; i++) {
 		for (let j = 0; j < rows; j++) {
-			grid[i][j].show();
+			const cell = grid[i][j];
+			if (!pause) {
+				const neighbourCells = getNeighbourCells(cell);
+				const nbrOfAliveNeighbourCells = evaluateNeighbourCells(neighbourCells);
+				cell.update(nbrOfAliveNeighbourCells);
+			}
+			cell.show();
 		}
 	}
 }
@@ -72,20 +134,29 @@ function setup() {
 
 	createCanvas(width, height);
 	strokeWeight(2);
+	background(255);
 
-	for (let i = 0; i <= columns; i++) {
-		line(width / columns * i, 0, width / columns * i, height);
-		for (let j = 0; j <= rows; j++) {
-			line(0, height / rows * j, width, height / rows * j);
-		}
-	}
+	pauseButton = createButton(!pause ? 'stop' : 'start');
+	pauseButton.mousePressed(function () { pause = !pause; });
+
+	// for (let i = 0; i <= columns; i++) {
+	// 	line(width / columns * i, 0, width / columns * i, height);
+	// 	for (let j = 0; j <= rows; j++) {
+	// 		line(0, height / rows * j, width, height / rows * j);
+	// 	}
+	// }
 }
 
-function mousePressed() {
+function changeCellState() {
 	const clickedCell = findCell(mouseX, mouseY);
 	if (clickedCell) clickedCell.changeState();
 }
 
+function mousePressed() {
+	changeCellState();
+}
+
 function draw() {
+	// pauseButton.text(!pause ? 'stop' : 'start');
 	updateGrid();
 }
